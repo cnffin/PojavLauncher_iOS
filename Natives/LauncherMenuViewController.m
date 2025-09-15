2ef11d0191185736a21bad843a4aa1354835ecf5
cnffin <93877815+cnffin@users.noreply.github.com>
2025-09-15 16:47:55 +0100

    Revert "attempting to fix ui"
    This reverts commit 6999f23fbc46939c00908f39edbac3e38890818a.

--- Natives/LauncherMenuViewController.m
+++ Natives/LauncherMenuViewController.m
@@ -54,10 +54,10 @@
     
     self.isInitialVc = YES;
     
-#UIImageView *titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AppLogo"]];
-#[titleView setContentMode:UIViewContentModeScaleAspectFit];
-#self.navigationItem.titleView = titleView;
-#[titleView sizeToFit];
+    UIImageView *titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AppLogo"]];
+    [titleView setContentMode:UIViewContentModeScaleAspectFit];
+    self.navigationItem.titleView = titleView;
+    [titleView sizeToFit];
     
     self.options = @[
         [LauncherMenuCustomItem vcClass:LauncherNewsViewController.class],
