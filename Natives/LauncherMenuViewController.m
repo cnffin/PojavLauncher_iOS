1a3e133f30a6183df1cf6fea947692974e680106
cnffin <93877815+cnffin@users.noreply.github.com>
2025-09-15 16:47:48 +0100

    Revert "Update LauncherMenuViewController.m"
    This reverts commit 590b55c5edc5c540adf3d1f857d1723ab73696c3.

--- Natives/LauncherMenuViewController.m
+++ Natives/LauncherMenuViewController.m
@@ -54,6 +54,11 @@
     
     self.isInitialVc = YES;
     
+#UIImageView *titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AppLogo"]];
+#[titleView setContentMode:UIViewContentModeScaleAspectFit];
+#self.navigationItem.titleView = titleView;
+#[titleView sizeToFit];
+    
     self.options = @[
         [LauncherMenuCustomItem vcClass:LauncherNewsViewController.class],
         [LauncherMenuCustomItem vcClass:LauncherProfilesViewController.class],
